740cd3456e4d5891de41792a426e523a281c4d5e
Johannes Olofsson <johannesolofsson@hotmail.com>
2017-12-17 19:22:19 +0100

    Work with tokens

---
+++ BearerTokenAuthentication/Model/LoginInputModel.cs
@@ -0,0 +1,9 @@
+﻿
+namespace BearerTokenAuthentication.Model
+{
+    public class LoginInputModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ BearerTokenAuthentication/Model/Todo.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BearerTokenAuthentication.Model
+{
+    public class Todo
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Name { get; set; }
+
+        [Required]
+        public bool IsComplete { get; set; }
+    }
+}
